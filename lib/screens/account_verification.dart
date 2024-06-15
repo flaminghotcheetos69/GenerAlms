3bb915d4b3fa3bcac8976bc222b36f64e67288e3
AuwesB <baigsuhail12345@gmai.com>
2024-06-16 02:00:31 +0800

    new auwes

--- lib/screens/account_verification.dart
+++ lib/screens/account_verification.dart
@@ -1,14 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart'; // Import Firestore
 
-class AccountVerificationPage extends StatelessWidget {
+class AccountVerificationPage extends StatefulWidget {
+  final String userType;
+
+  AccountVerificationPage({required this.userType});
+
+  @override
+  _AccountVerificationPageState createState() =>
+      _AccountVerificationPageState();
+}
+
+class _AccountVerificationPageState extends State<AccountVerificationPage> {
+  File? _image;
+
+  Future<void> _pickImage() async {
+    final pickedFile =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+      });
+    }
+  }
+
+  Future<String?> _uploadImage() async {
+    if (_image != null) {
+      final storageRef = FirebaseStorage.instance.ref().child(
+          'verificationApplications/${widget.userType}/${DateTime.now().toIso8601String()}.jpg');
+      await storageRef.putFile(_image!);
+      String imageUrl = await storageRef.getDownloadURL();
+      return imageUrl; // Return imageUrl from upload method
+    }
+    return null;
+  }
+
+  Future<void> _storeImageUrlInFirestore(String imageUrl) async {
+    // Update Firestore with the imageUrl
+    try {
+      await FirebaseFirestore.instance
+          .collection('verificationApplications')
+          .doc(widget.userType)
+          .set({'imageUrl': imageUrl}, SetOptions(merge: true));
+      print('Image URL stored in Firestore.');
+    } catch (e) {
+      print('Error storing image URL: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Account Verification'),
+        title: Text('Account Verification (${widget.userType})'),
       ),
-      body: Center(
-        child: Text('This is the account verification page.'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            _image == null ? Text('No image selected.') : Image.file(_image!),
+            ElevatedButton(
+              onPressed: _pickImage,
+              child: Text('Pick Image'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                String? imageUrl = await _uploadImage();
+                if (imageUrl != null) {
+                  await _storeImageUrlInFirestore(imageUrl);
+                }
+              },
+              child: Text('Upload Image'),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -24,7 +24,8 @@ class _ProfilePageState extends State<ProfilePage> {
 
   void _fetchUserData() async {
     if (_user != null) {
-      DocumentSnapshot userDoc = await _firestore.collection('users').doc(_user!.uid).get();
+      DocumentSnapshot userDoc =
+          await _firestore.collection('users').doc(_user!.uid).get();
       setState(() {
         _userData = userDoc.data() as Map<String, dynamic>?;
       });
@@ -38,7 +39,10 @@ class _ProfilePageState extends State<ProfilePage> {
       setState(() {
         _userData!['profileImageUrl'] = image.path;
       });
-      _firestore.collection('users').doc(_user!.uid).update({'profileImageUrl': image.path});
+      _firestore
+          .collection('users')
+          .doc(_user!.uid)
+          .update({'profileImageUrl': image.path});
     }
   }
 
@@ -49,65 +53,69 @@ class _ProfilePageState extends State<ProfilePage> {
     }
 
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
+      ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Stack(
-              children: [
-                CircleAvatar(
-                  radius: 50,
-                  backgroundImage: _userData!['profileImageUrl'] != null
-                      ? NetworkImage(_userData!['profileImageUrl'])
-                      : null,
-                  child: _userData!['profileImageUrl'] == null
-                      ? Icon(Icons.person, size: 50)
-                      : null,
-                ),
-                Positioned(
-                  bottom: 0,
-                  right: 0,
-                  child: IconButton(
-                    icon: Icon(Icons.edit),
-                    onPressed: _pickImage,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 10),
-            Text(
-              _userData!['fullName'] ?? 'User Name',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 20),
-            ListTile(
-              leading: Icon(Icons.verified_user),
-              title: Text(_userData!['userType'] == 'Donator'
-                  ? 'Donator account verification'
-                  : 'Recipient account verification'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => AccountVerificationPage()),
-                );
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.lock),
-              title: Text('Privacy and security'),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: Icon(Icons.location_on),
-              title: Text('Location preferences'),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: Icon(Icons.history),
-              title: Text('Donee history'),
-              onTap: () {},
-            ),
-          ],
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: _userData!['profileImageUrl'] != null
+                    ? NetworkImage(_userData!['profileImageUrl'])
+                    : null,
+                child: _userData!['profileImageUrl'] == null
+                    ? Icon(Icons.person, size: 50)
+                    : null,
+              ),
+              SizedBox(height: 20),
+              Text(
+                _userData!['fullName'] ?? 'User Name',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 20),
+              ListTile(
+                leading: Icon(Icons.verified_user),
+                title: Text(_userData!['userType'] == 'Donator'
+                    ? 'Donator account verification'
+                    : 'Recipient account verification'),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => AccountVerificationPage(
+                        userType: _userData!['userType'],
+                      ),
+                    ),
+                  );
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.lock),
+                title: Text('Privacy and security'),
+                onTap: () {
+                  // Handle privacy and security tap
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.location_on),
+                title: Text('Location preferences'),
+                onTap: () {
+                  // Handle location preferences tap
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.history),
+                title: Text('Donee history'),
+                onTap: () {
+                  // Handle donee history tap
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
